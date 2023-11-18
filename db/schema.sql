d7085025c89088688749efda7cb0394e5f4e7fca
jdardoin <jdardoin@icloud.com>
2023-11-17 18:13:08 -0600

    update seeds

--- db/schema.sql
+++ db/schema.sql
@@ -7,23 +7,25 @@ USE employee_tracker_db;
 
 -- creating tables 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY(id),
-    name VARCHAR(30) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
    );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL (10, 2),
     department_id INT NOT NULL,
-    FOReIGN KEY (department_id) REFERENCES department(id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) on delete cascade
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) on delete cascade,
+    manager_id INT,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) on delete cascade
 
 );
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
-
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Connect to the database 
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+
+
+
+
+// Connect to the database 
 const db = mysql.createConnection(
     {
 
